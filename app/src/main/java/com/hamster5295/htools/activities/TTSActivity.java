58c82636e57b8ec8d91a041f840c46a87a72ab38
hamster5295 <hamster5295@163.com>
2022-01-16 11:17:11 +0800

    Move TTS method body to OnRequestPermissionsResult()

--- app/src/main/java/com/hamster5295/htools/activities/TTSActivity.java
+++ app/src/main/java/com/hamster5295/htools/activities/TTSActivity.java
@@ -1,5 +1,6 @@
 package com.hamster5295.htools.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -57,75 +58,86 @@ public class TTSActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }).start();
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         Button b = findViewById(R.id.btn_tts_go);
         EditText t = findViewById(R.id.et_tts),
                 fn = findViewById(R.id.et_tts_fileName);
         final TextView log = findViewById(R.id.text_tts_log);
 
-        b.setOnClickListener((v) -> new Thread(() -> {
-            try {
-                if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-                }
+        if(grantResults.length >0){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                b.setOnClickListener((v) -> new Thread(() -> {
+                    try {
+                        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                        }
 
-                OkHttpClient client = new OkHttpClient.Builder()
-                        .callTimeout(5000, TimeUnit.MILLISECONDS)
-                        .build();
+                        OkHttpClient client = new OkHttpClient.Builder()
+                                .callTimeout(5000, TimeUnit.MILLISECONDS)
+                                .build();
 
-                FormBody body = new FormBody.Builder()
-                        .add("tex", URLEncoder.encode(t.getText().toString(), "utf-8"))
-                        .add("tok", GlobalData.accessToken)
-                        .add("cuid", "qwq")
-                        .add("ctp", "1")
-                        .add("lan", "zh")
-                        .add("spd", "5")
-                        .add("per", "0")
-                        .add("aue", "3")
-                        .build();
+                        FormBody body = new FormBody.Builder()
+                                .add("tex", URLEncoder.encode(t.getText().toString(), "utf-8"))
+                                .add("tok", GlobalData.accessToken)
+                                .add("cuid", "qwq")
+                                .add("ctp", "1")
+                                .add("lan", "zh")
+                                .add("spd", "5")
+                                .add("per", "0")
+                                .add("aue", "3")
+                                .build();
 
-                Request request = new Request.Builder()
-                        .url("http://tsn.baidu.com/text2audio")//访问连接
-                        .post(body)
-                        .build();
+                        Request request = new Request.Builder()
+                                .url("http://tsn.baidu.com/text2audio")//访问连接
+                                .post(body)
+                                .build();
 
-                Call call = client.newCall(request);
-                Response response = call.execute();
-                if (response.isSuccessful()) {
-                    File f = new File(Environment.getExternalStorageDirectory(), "HTools/TTS/");
-                    f.mkdirs();
-                    FileOutputStream os = new FileOutputStream(f.getPath() + "/" + fn.getText().toString() + ".mp3");
-                    os.write(response.body().bytes());
-                    os.flush();
-                    os.close();
-                    log.setText("保存成功!\n目录: " + f.getPath() + "/"+ fn.getText().toString() + ".mp3");
-                } else {
-                    switch (response.code()) {
-                        case 501:
-                            log.setText("超时");
-                            break;
+                        Call call = client.newCall(request);
+                        Response response = call.execute();
+                        if (response.isSuccessful()) {
+                            File f = new File(Environment.getExternalStorageDirectory(), "HTools/TTS/");
+                            f.mkdirs();
+                            FileOutputStream os = new FileOutputStream(f.getPath() + "/" + fn.getText().toString() + ".mp3");
+                            os.write(response.body().bytes());
+                            os.flush();
+                            os.close();
+                            log.setText("保存成功!\n目录: " + f.getPath() + "/"+ fn.getText().toString() + ".mp3");
+                        } else {
+                            switch (response.code()) {
+                                case 501:
+                                    log.setText("超时");
+                                    break;
 
-                        case 502:
-                            log.setText("参数错误");
-                            break;
+                                case 502:
+                                    log.setText("参数错误");
+                                    break;
 
-                        case 503:
-                            log.setText("Token无效");
-                            break;
+                                case 503:
+                                    log.setText("Token无效");
+                                    break;
 
-                        case 504:
-                            log.setText("文本编码有误");
-                            break;
+                                case 504:
+                                    log.setText("文本编码有误");
+                                    break;
 
-                        default:
-                            log.setText(response.body().string());
-                            break;
+                                default:
+                                    assert response.body() != null;
+                                    log.setText(response.body().string());
+                                    break;
+                            }
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+                }).start());
+            }else{
+                log.setText(R.string.no_permission);
             }
-        }).start());
+        }else{
+            log.setText(R.string.no_permission);
+        }
     }
-
 }
